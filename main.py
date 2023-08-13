68995d57d763cf94d9679051885dee145d0fd569
Sunan <sunan@seas.upenn.edu>
2023-08-13 15:23:44 -0400

    checkpoint

--- main.py
+++ main.py
@@ -1,39 +1,60 @@
 import sys, os
 import numpy as np
 from scipy.io import loadmat
+from process_bag import process_bag_file
 
 dir_path     = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(dir_path, 'damm'))
-sys.path.append(os.path.join(dir_path, 'ds_opt'))
+sys.path.append(os.path.join(dir_path, 'ds_opt_ood'))
 
 
 from damm.main import damm
-from ds_opt.utils_ds import load_tools
-from ds_opt.main import DsOpt
+from ds_opt_ood.utils_ds import load_tools
+from ds_opt_ood.main import DsOpt
+from ds_opt_ood.utils_ds.plotting_tool.plot_reference_trajectories_DS import plot_reference_trajectories_DS
+ 
 
 # Define Path
 input_path    = os.path.join(dir_path, 'input.mat')
 output_path   = os.path.join(dir_path, 'output.json')
 
 
-dim = 2
-# Load Data
-if dim ==2:
-    data_ = loadmat(r"{}".format(input_path))
-    data_ = np.array(data_["data"])
-    N = len(data_[0])
-    input_data = data_.reshape((N, 1))
-else:
-    data_ = loadmat(r"{}".format(input_path))
-    data_ = np.array(data_["data"])
-    N = len(data_)
-    # traj = np.random.choice(np.arange(N), 4, replace=False)
-    traj = np.array([6, 8, 3, 5]) - 1
-    input_data = data_[traj]
+# dim = 2
+# # Load Data
+# if dim ==2:
+#     data_ = loadmat(r"{}".format(input_path))
+#     data_ = np.array(data_["data"])
+#     N = len(data_[0])
+#     input_data = data_.reshape((N, 1))
+# else:
+#     data_ = loadmat(r"{}".format(input_path))
+#     data_ = np.array(data_["data"])
+#     N = len(data_)
+#     # traj = np.random.choice(np.arange(N), 4, replace=False)
+#     traj = np.array([6, 8, 3, 5]) - 1
+#     input_data = data_[traj]
+
+
+input_path    = os.path.join(dir_path, 'all.mat')
+
+input_data = process_bag_file(input_path)
+print(input_data)
 
 Data, Data_sh, att, x0_all, dt, _, traj_length = load_tools.processDataStructure(input_data)
 
 
+att = np.array(att).reshape(3, 1)
+plot_reference_trajectories_DS(Data, att, 100, 20)
+
+print(Data.shape)
+import matplotlib.pyplot as plt
+
+plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter(Data[0, :], Data[1, :], Data[2, :], c='k', s=5)
+
+
+plt.show()
 # Init dict
 data_dict = {
     "Data": Data,
@@ -45,13 +66,13 @@ data_dict = {
 }
 
 
-# Run DAMM
+# # Run DAMM
 DAMM = damm(Data)         
 if DAMM.begin() == 0:
     DAMM.result(if_plot=True)
 
 
-# Run ds_opt and output JSON
+# # Run ds_opt and output JSON
 ds_opt = DsOpt(data_dict, os.path.join(dir_path, "output.json"))
 ds_opt.begin()
 ds_opt.evaluate()
---
+++ process_bag.py
@@ -0,0 +1,54 @@
+import numpy as np
+import os, sys
+from scipy.io import loadmat
+
+
+
+def process_bag_file(path):
+
+    data_ = loadmat(r"{}".format(path))
+
+    data_ = data_['data_ee_pose']
+    L = data_.shape[1]
+
+    # full_traj = np.empty((L, ), dtype=object)
+    output_traj = np.empty((L, 1), dtype=object)
+
+    sample_step =1
+    vel_thresh = 3e-3 
+    
+    for l in range(L):
+        data_l = data_[0, l]['pose'][0,0]
+        pos_traj  = data_l[:3, ::sample_step]
+        time_traj = data_l[-1, ::sample_step].reshape(1,-1)
+
+        raw_diff_pos = np.diff(pos_traj)
+        vel_mag = np.linalg.norm(raw_diff_pos, axis=0).flatten()
+        first_non_zero_index = np.argmax(vel_mag > vel_thresh)
+        last_non_zero_index = len(vel_mag) - 1 - np.argmax(vel_mag[::-1] > vel_thresh)
+
+        if first_non_zero_index >= last_non_zero_index:
+            raise Exception("Sorry, vel are all zero")
+
+        pos_traj = pos_traj[:, first_non_zero_index:last_non_zero_index]
+        time_traj = time_traj[:, first_non_zero_index:last_non_zero_index]
+
+        pos_diff_traj = np.diff(pos_traj)
+        time_diff_traj = np.diff(time_traj)
+
+        vel_traj = pos_diff_traj / time_diff_traj
+        
+        output_traj[l, 0] = np.vstack((pos_traj[:, 0:-1], vel_traj))
+
+    return output_traj
+
+
+
+if __name__ == '__main__':
+
+    dir_path     = os.path.dirname(os.path.realpath(__file__))
+    input_path    = os.path.join(dir_path, 'all.mat')
+
+    output_traj = process_bag_file(input_path)
+
+    print(output_traj.shape)
