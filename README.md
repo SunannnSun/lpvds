fd0d0c85d416ebfb1486a400883c404f3b1fe205
Sunan Sun <sunan@seas.upenn.edu>
2023-08-19 16:07:12 -0400

    made a util folder for loading and processing data; rearrange the dataset folder

--- README.md
+++ README.md
@@ -6,30 +6,26 @@ Usage
 
 ```
 git clone https://github.com/SunannnSun/damm_lpvds.git
-```
-```
 cd damm_lpvds
-```
-```
 git clone https://github.com/SunannnSun/damm.git
-```
-```
 git clone https://github.com/HuiTakami/ds_opt_ood.git
 ```
 
 
+Create a python virtual environment and install the dependencies.
 
-Start a virtual environment
 ```
-virtualenv env3.8
-```
-```
+virtualenv -p /path/to/python3.8 env3.8
 source env3.8/bin/activate
+pip install -r requirements.txt
 ```
 
-Install dependency
+Make sure to replace `/path/to/python3.8` with the correct path to the Python 3.8 executable on your system. You can use the which command on Unix-like systems or the where command on Windows to find the path to the Python interpreter. For example:
+
+On Unix-like systems (Linux, macOS):
+
 ```
-pip install -r damm/requirements.txt
+which python3.8
 ```
 
 Compile damm module
--- main.py
+++ main.py
@@ -2,28 +2,31 @@ import sys, os
 import numpy as np
 from scipy.io import loadmat
 from scipy.io import savemat 
-from process_bag import process_bag_file
 import pyLasaDataset as lasa
 
-dir_path     = os.path.dirname(os.path.realpath(__file__))
-# sys.path.append(os.path.join(dir_path, 'damm'))
-sys.path.append(os.path.join(dir_path, 'ds_opt_ood'))
+
 
 
 from damm.damm import damm as damm_class
-from ds_opt_ood.utils_ds import load_tools
-# from ds_opt_ood.main import DsOpt
-from ds_opt_ood.utils_ds.plotting_tool.plot_reference_trajectories_DS import plot_reference_trajectories_DS
- 
-# Temporary input method: 
-# 1. loading PC-GMM benchmark dataset; must be named input.mat and placed under the same directory
-# 2. loading LASA benchmark dataset; need to specify the data by changing the name in data=lasa.DataSet.name
-# 3. loading real trajectory collected during rebuttal, must be named all.mat and placed under the same directory
+from util import load_tools, process_bag, plot_reference_trajectories_DS
 
-input_method = 3
+ 
+# sys.path.append(os.path.join(dir_path, 'ds_opt_ood'))
+# from ds_opt_ood.main import DsOpt as dsopt_class
+
+
+'''
+#############################################  Input method ###############################################
+#### 1. PC-GMM benchmark dataset; must be named input.mat and placed under the same directory #############
+#### 2. LASA benchmark dataset; need to specify the data by changing the name in data=lasa.DataSet.name ###
+#### 3. DAMM demo data; must be named all.mat and placed under the same directory #########################
+'''
+
+
+input_method = 1
+dir_path     = os.path.dirname(os.path.realpath(__file__))
 
 if input_method == 1:
-    # Define Path
     input_path    = os.path.join(dir_path, 'input.mat')
     output_path   = os.path.join(dir_path, 'output.json')
 
@@ -34,13 +37,13 @@ if input_method == 1:
         data_ = np.array(data_["data"])
         N = len(data_[0])
         input_data = data_.reshape((N, 1))
-    # else:
-    #     data_ = loadmat(r"{}".format(input_path))
-    #     data_ = np.array(data_["data"])
-    #     N = len(data_)
-    #     # traj = np.random.choice(np.arange(N), 4, replace=False)
-    #     traj = np.array([6, 8, 3, 5]) - 1
-    #     input_data =
+    else:
+        data_ = loadmat(r"{}".format(input_path))
+        data_ = np.array(data_["data"])
+        N = len(data_)
+        # traj = np.random.choice(np.arange(N), 4, replace=False)
+        traj = np.array([6, 8, 3, 5]) - 1
+        input_data = data_[traj]
 
 
 elif input_method ==2:
@@ -60,50 +63,36 @@ elif input_method ==2:
         input_data[l, 0] = np.vstack((pos, vel))
 
 elif input_method == 3:
-
     input_path    = os.path.join(dir_path, 'all.mat')
-    input_data = process_bag_file(input_path)
-    input_data_copy = input_data.copy()
+    input_data = process_bag.process_bag_file(input_path)
 
 
 Data, Data_sh, att, x0_all, dt, _, traj_length = load_tools.processDataStructure(input_data)
 
 
-# att = np.array(att).reshape(3, 1)
-# plot_reference_trajectories_DS(Data, att, 100, 20)
-
-# print(Data.shape)
-# import matplotlib.pyplot as plt
-
-# plt.figure()
-# ax = plt.axes(projection='3d')
-# ax.scatter(Data[0, :], Data[1, :], Data[2, :], c='k', s=5)
-
-
+# plot_reference_trajectories_DS.plot_reference_trajectories_DS(Data, att, 100, 20)
 # plt.show()
+
+
+DAMM = damm_class(Data)         
+if DAMM.begin() == 0:
+    DAMM.result(if_plot=True)
+
+
+
 data_dict = {
     "Data": Data,
     "Data_sh": Data_sh,
-    "att": att,
+    "att": np.array(att).reshape(3, 1),
     "x0_all": x0_all,
     "dt": dt,
     "traj_length":traj_length
 }
 
 
-# # Run DAMM
-DAMM = damm_class(Data)         
-if DAMM.begin() == 0:
-    DAMM.result(if_plot=True)
-
-# Priors = DAMM.Priors
-# Mu = DAMM.Mu
-# Sigma = DAMM.Sigma
-
-
 """
 # # Run ds_opt and output JSON
-ds_opt = DsOpt(data_dict, os.path.join(dir_path, "output.json"))
+ds_opt = dsopt_class(data_dict, os.path.join(dir_path, "output.json"))
 ds_opt.begin()
 ds_opt.evaluate()
 ds_opt.make_plot()
@@ -114,10 +103,10 @@ ds_opt.make_plot()
 
 
 # damm_output = {
-#     'cov': Sigma,
-#     'mu': Mu,
-#     'pi': Priors,
-#     'traj': input_data_copy
+#     'cov': DAMM.Sigma,
+#     'mu': DAMM.Mu,
+#     'pi': DAMM.Priors,
+#     'traj': input_data.copy()
 # }
 # savemat('damm_output.mat', damm_output)
 
